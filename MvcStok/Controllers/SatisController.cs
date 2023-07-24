94db960ab9bc88f9bd9671edd27dd7de33d8569d
kandemiryazilim <kandemir.yazilim@gmail.com>
2023-07-24 15:49:02 +0300

    Add project files.

---
+++ MvcStok/Controllers/SatisController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+
+namespace MvcStok.Controllers
+{
+    public class SatisController : Controller
+    {
+        // GET: Satis
+        MvcDbStokEntities1 db = new MvcDbStokEntities1();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult YeniSatis()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult YeniSatis(TBLSATISLAR p)
+        {
+            db.TBLSATISLAR.Add(p);
+            db.SaveChanges();
+            return View("Index");
+
+
+        }
+        
+    }
+}
